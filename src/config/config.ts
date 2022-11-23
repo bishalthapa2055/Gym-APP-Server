407bfb88f3b9130950455acd2a0dfda1365745f5
Bishal Thapa <bishalthapab989@gmail.com>
2022-11-23 17:41:29 +0545

    dep

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // const MONGO_URL = process.env.MONGO_URL;
-const MONGO_URL = "mongodb://localhost:27017/gymm";
+const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 8888;
 
 export const config = {
